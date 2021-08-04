2dfd181bb6b5318721d2332459f9ff1dfa86a623
Giancarlo Tricerri <giankitricerri@gmail.com>
2021-08-04 10:10:12 -0400

    [feat] added unit test for user listing

--- src/main/java/com/citrus/backoffice/user/app/UserService.java
+++ src/main/java/com/citrus/backoffice/user/app/UserService.java
@@ -2,7 +2,7 @@ package com.citrus.backoffice.user.app;
 
 import java.util.List;
 
-import com.citrus.backoffice.interview.domain.Interview;
+
 import com.citrus.backoffice.shared.domain.User;
 
 public interface UserService {
